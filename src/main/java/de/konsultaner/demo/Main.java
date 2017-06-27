2024ea23d4b7b725278804e506581815d52e7321
Richard Burkhardt <burkhardt@konsultaner.de>
2017-06-27 18:23:56 +0200

    working rainbow

--- src/main/java/de/konsultaner/demo/Main.java
+++ src/main/java/de/konsultaner/demo/Main.java
@@ -31,57 +31,13 @@ public class Main {
                 int green = calculateColorValue(((((i-rampLength)%ledSize)+ledSize)%ledSize),rampLength);
                 int blue = calculateColorValue(((((i-rampLength*2)%ledSize)+ledSize)%ledSize),rampLength);
                 pixelHandler.getPixelChannel(0).setPixel(i, 0, Pixel.fromColor(white,red,green,blue));
-                System.out.println("Pixel"+(i+1)+":"+white+","+red+","+green+","+blue);
             }
             pixelHandler.render();
-            Thread.sleep(3000);
-
-            pixelHandler.getPixelChannel(0).setPixel(0, 0, Pixel.fromColor(255,0,0,0));
-            pixelHandler.getPixelChannel(0).setPixel(2, 0, Pixel.fromColor(0,255,0,0));
-            pixelHandler.getPixelChannel(0).setPixel(4, 0, Pixel.fromColor(0,0,255,0));
-            pixelHandler.getPixelChannel(0).setPixel(16, 0, Pixel.fromColor(0,0,0,255));
-
-            pixelHandler.render();
-            System.out.println("1");
-            Thread.sleep(1000);
-            pixelHandler.getPixelChannel(0).setPixel(1, 0, Pixel.fromColor(255,0,0,0));
-            pixelHandler.getPixelChannel(0).setPixel(3, 0, Pixel.fromColor(0,255,0,0));
-            pixelHandler.getPixelChannel(0).setPixel(5, 0, Pixel.fromColor(0,0,255,0));
-            pixelHandler.getPixelChannel(0).setPixel(17, 0, Pixel.fromColor(0,0,0,255));
-            pixelHandler.render();
-            System.out.println("2");
-            Thread.sleep(1000);
-            pixelHandler.getPixelChannel(0).setPixel(1, 0, Pixel.fromColor(255,255,255,255));
-            pixelHandler.getPixelChannel(0).setPixel(3, 0, Pixel.fromColor(0,0,255,0));
-            pixelHandler.getPixelChannel(0).setPixel(5, 0, Pixel.fromColor(0,0,0,255));
-            pixelHandler.getPixelChannel(0).setPixel(17, 0, Pixel.fromColor(255,0,0,0));
-            pixelHandler.render();
-            System.out.println("2");
-            Thread.sleep(1000);
-            pixelHandler.getPixelChannel(0).setPixel(1, 0, Pixel.fromColor(0,0,0,255));
-            pixelHandler.getPixelChannel(0).setPixel(3, 0, Pixel.fromColor(0,0,255,0));
-            pixelHandler.getPixelChannel(0).setPixel(5, 0, Pixel.fromColor(0,0,0,255));
-            pixelHandler.getPixelChannel(0).setPixel(17, 0, Pixel.fromColor(255,0,0,0));
-            pixelHandler.getPixelChannel(0).setPixel(99, 0, Pixel.fromColor(255,255,255,255));
-            pixelHandler.render();
-            System.out.println("3");
-            Thread.sleep(5000);
-            pixelHandler.getPixelChannel(0).setPixel(1, 0, Pixel.fromColor(0,0,0,0));
-            pixelHandler.getPixelChannel(0).rightShiftPixelsRow(0);
-            pixelHandler.render();
-            System.out.println("4");
-            Thread.sleep(5000);
-            pixelHandler.getPixelChannel(0).setPixel(1, 0, Pixel.fromColor(0,0,0,0));
-            pixelHandler.getPixelChannel(0).rightShiftPixelsRow(0);
-            pixelHandler.render();
-            System.out.println("5");
-            Thread.sleep(5000);
-
-            
-            /*for (int i = 0; i < ledSize; i++) {
+            for (int i = 0; i < ledSize; i++) {
+                pixelHandler.getPixelChannel(0).rightShiftPixelsRow(0);
                 pixelHandler.render();
                 Thread.sleep(1000/15);
-            }*/
+            }
             pixelHandler.getPixelChannel(0).clearChannel();
             pixelHandler.render();
 
